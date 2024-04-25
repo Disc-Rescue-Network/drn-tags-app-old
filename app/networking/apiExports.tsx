bedf466581ba42d2c6ac22eb9dc6a3781750e636
Chris Deck <deckchris95@gmail.com>
2024-04-25 18:30:54 -0400

    no events yet

--- app/networking/apiExports.tsx
+++ app/networking/apiExports.tsx
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "https://api.discrescuenetwork.com"; //production URL
 //export const API_BASE_URL = "http://127.0.0.1:3000"; // local testing
 
-//export const TAGSAPI_BASE_URL = "https://api.discrescuenetwork.com"; //production URL
-export const TAGS_API_BASE_URL = "http://127.0.0.1:3001"; // local testing
+export const TAGS_API_BASE_URL = "https://tags-api.discrescuenetwork.com"; //production URL
+//export const TAGS_API_BASE_URL = "http://127.0.0.1:3001"; // local testing
