9ba7fd9c011888aa1309edd8787a2d25d9d07f84
Chris Deck <deckchris95@gmail.com>
2024-07-16 18:11:29 -0400

    layout - course settings

--- app/admin/course-settings/page.tsx
+++ app/admin/course-settings/page.tsx
@@ -50,6 +50,16 @@ import { TAGS_API_BASE_URL } from "@/app/networking/apiExports";
 import { KindeOrganization } from "@kinde-oss/kinde-auth-nextjs/types";
 import { Label } from "@/components/ui/label";
 import { set } from "date-fns";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { PlusCircle } from "lucide-react";
 
 const fetchCourseSettings = async (orgCode: KindeOrganization) => {
   // console.log("Fetching course settings for orgCode:", orgCode);
@@ -266,179 +276,238 @@ const AdminTools: NextPage = () => {
     }
   };
 
-  if (isLoading && !user) return <div>Loading...</div>;
+  // if (isLoading && !user) return <div>Loading...</div>;
 
   return (
     <div className="relative grid grid-cols-1 text-left items-start justify-start gap-4 p-6">
-      {loading && (
-        <div className="absolute bg-black bg-opacity-60 z-10 h-dvh w-full flex items-center justify-center">
-          <div className="flex items-center">
-            <svg
-              aria-hidden="true"
-              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-      )}
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          {/* Course Name */}
-          <FormField
-            control={form.control}
-            name="courseName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Course Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="Course Name" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
+      {loading ? (
+        <Skeleton className="w-full h-72" />
+      ) : (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            {/* Course Name */}
+            <FormField
+              control={form.control}
+              name="courseName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Course Name" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
 
-          {/* Course Short Code */}
-          <FormField
-            control={form.control}
-            name="shortCode"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Course Short Code</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="Course Short Code (i.e., TRANQ)"
-                    onChange={(e) => {
-                      e.target.value = e.target.value.toUpperCase();
-                      field.onChange(e);
-                    }}
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-
-          {/* City */}
-          <FormField
-            control={form.control}
-            name="city"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>City</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="City" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-
-          {/* State */}
-          <FormField
-            control={form.control}
-            name="state"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>State</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="State" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-
-          {fields.map((field, index) => (
-            <div key={field.id}>
-              <FormField
-                control={form.control}
-                name={`layouts.${index}.name`}
-                render={({ field }) => (
-                  <div className="flex flex-row gap-1 items-end w-full">
-                    <FormItem>
-                      <FormLabel>Layout {index + 1}</FormLabel>
-                      <FormControl>
-                        <Input {...field} placeholder={`Layout ${index + 1}`} />
-                      </FormControl>
-                    </FormItem>
-                    <Button type="button" onClick={() => remove(index)}>
-                      Remove
-                    </Button>
-                  </div>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name={`layouts.${index}.par`}
-                render={({ field }) => (
-                  <div className="flex flex-row gap-1 items-end w-full">
-                    <FormItem>
-                      <FormLabel>Par {index + 1}</FormLabel>
-                      <FormControl>
-                        <Input {...field} placeholder={`Par ${index + 1}`} />
-                      </FormControl>
-                    </FormItem>
-                  </div>
-                )}
-              />
-            </div>
-          ))}
-          <Button type="button" onClick={() => append({ name: "", par: "-1" })}>
-            + Add Layout
-          </Button>
-
-          {/* Starting Holes */}
-          <div className="flex flex-col gap-2 text-left w-full">
-            <h1>Preferred Starting Holes</h1>
-            <Label className="text-sm text-gray-500">
-              Note: Each event may override this setting.
-            </Label>
-            <div className="grid grid-cols-3 gap-2 justify-center items-center">
-              {form
-                .watch("holes")
-                .sort(
-                  (a: { hole_number: number }, b: { hole_number: number }) =>
-                    a.hole_number - b.hole_number
-                )
-                .map(
-                  (
-                    hole: { hole_id: Key | null | undefined; hole_number: any },
-                    index: any
-                  ) => (
-                    <FormField
-                      key={hole.hole_id}
-                      control={form.control}
-                      name={`holes.${index}.active`}
-                      render={({ field }) => (
-                        <FormItem className="grid grid-cols-2 gap-2 w-full">
-                          <FormLabel className="min-w-fit text-sm items-center justify-center">{`Hole ${hole.hole_number}`}</FormLabel>
-                          <FormControl>
-                            <Checkbox
-                              checked={field.value}
-                              onCheckedChange={field.onChange}
-                              className="h-4 w-4 !mt-[2px] !mb-0 items-center justify-center"
-                            />
-                          </FormControl>
-                        </FormItem>
-                      )}
+            {/* Course Short Code */}
+            <FormField
+              control={form.control}
+              name="shortCode"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Short Code</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Course Short Code (i.e., TRANQ)"
+                      onChange={(e) => {
+                        e.target.value = e.target.value.toUpperCase();
+                        field.onChange(e);
+                      }}
                     />
-                  )
-                )}
-            </div>
-          </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
 
-          {/* Venmo and CashApp Usernames */}
-          {/* <FormField
+            {/* City */}
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="City" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            {/* State */}
+            <FormField
+              control={form.control}
+              name="state"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>State</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="State" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <Card>
+              <CardHeader>
+                <CardTitle>Layouts</CardTitle>
+                <CardDescription>
+                  Add and remove layouts for this course.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Layout Name</TableHead>
+                      <TableHead>Par</TableHead>
+                      <TableHead className="w-[100px]"></TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {fields.map((field, index) => (
+                      <TableRow key={field.id}>
+                        <TableCell>
+                          <Label
+                            htmlFor={`layout-name-${index}`}
+                            className="sr-only"
+                          >
+                            Layout Name
+                          </Label>
+                          <Input
+                            id={`layout-name-${index}`}
+                            {...register(`layouts.${index}.name`)}
+                            placeholder={`Layout ${index + 1}`}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <Label htmlFor={`par-${index}`} className="sr-only">
+                            Par
+                          </Label>
+                          <Input
+                            id={`par-${index}`}
+                            {...register(`layouts.${index}.par`)}
+                            placeholder="Par"
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <Button
+                            type="button"
+                            variant="destructive"
+                            className="gap-1"
+                            onClick={() => remove(index)}
+                          >
+                            Remove
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardContent>
+              <CardFooter className="justify-center border-t p-4">
+                <Button
+                  type="button"
+                  size="sm"
+                  variant="ghost"
+                  className="gap-1"
+                  onClick={() => append({ name: "", par: "-1" })}
+                >
+                  <PlusCircle className="h-3.5 w-3.5" />
+                  Add Layout
+                </Button>
+              </CardFooter>
+            </Card>
+            {/* {fields.map((field, index) => (
+              <div key={field.id}>
+                <FormField
+                  control={form.control}
+                  name={`layouts.${index}.name`}
+                  render={({ field }) => (
+                    <div className="flex flex-row gap-1 items-end w-full">
+                      <FormItem>
+                        <FormLabel>Layout {index + 1}</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder={`Layout ${index + 1}`}
+                          />
+                        </FormControl>
+                      </FormItem>
+                      <Button type="button" onClick={() => remove(index)}>
+                        Remove
+                      </Button>
+                    </div>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name={`layouts.${index}.par`}
+                  render={({ field }) => (
+                    <div className="flex flex-row gap-1 items-end w-full">
+                      <FormItem>
+                        <FormLabel>Par {index + 1}</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder={`Par ${index + 1}`} />
+                        </FormControl>
+                      </FormItem>
+                    </div>
+                  )}
+                />
+              </div>
+            ))}
+            <Button
+              type="button"
+              onClick={() => append({ name: "", par: "-1" })}
+            >
+              + Add Layout
+            </Button> */}
+
+            {/* Starting Holes */}
+            <div className="flex flex-col gap-2 text-left w-full">
+              <h1>Preferred Starting Holes</h1>
+              <Label className="text-sm text-gray-500">
+                Note: Each event may override this setting.
+              </Label>
+              <div className="grid grid-cols-3 gap-2 justify-center items-center">
+                {form
+                  .watch("holes")
+                  .sort(
+                    (a: { hole_number: number }, b: { hole_number: number }) =>
+                      a.hole_number - b.hole_number
+                  )
+                  .map(
+                    (
+                      hole: {
+                        hole_id: Key | null | undefined;
+                        hole_number: any;
+                      },
+                      index: any
+                    ) => (
+                      <FormField
+                        key={hole.hole_id}
+                        control={form.control}
+                        name={`holes.${index}.active`}
+                        render={({ field }) => (
+                          <FormItem className="grid grid-cols-2 gap-2 w-full">
+                            <FormLabel className="min-w-fit text-sm items-center justify-center">{`Hole ${hole.hole_number}`}</FormLabel>
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                                className="h-4 w-4 !mt-[2px] !mb-0 items-center justify-center"
+                              />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                    )
+                  )}
+              </div>
+            </div>
+
+            {/* Venmo and CashApp Usernames */}
+            {/* <FormField
             control={form.control}
             name="venmoUsername"
             render={({ field }) => (
@@ -463,40 +532,41 @@ const AdminTools: NextPage = () => {
             )}
           /> */}
 
-          {/* Divisions */}
-          <div className="flex flex-col gap-2 justify-start items-start">
-            <h1>Divisions for Tags</h1>
-            <div className="grid grid-cols-4 gap-2 justify-center items-center">
-              {form
-                .watch("divisions")
-                .sort((a, b) => a.division_id - b.division_id)
-                .map((division, index) => (
-                  <FormField
-                    key={division.division_id}
-                    control={form.control}
-                    name={`divisions.${index}.active`}
-                    render={({ field }) => (
-                      <FormItem className="grid grid-cols-2 gap-2 w-full">
-                        <FormLabel className="min-w-fit text-sm items-center justify-center">
-                          {division.name}
-                        </FormLabel>
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value}
-                            onCheckedChange={field.onChange}
-                            className="h-4 w-4 !mt-[2px] !mb-0 items-center justify-center"
-                          />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
-                ))}
+            {/* Divisions */}
+            <div className="flex flex-col gap-2 justify-start items-start">
+              <h1>Divisions for Tags</h1>
+              <div className="grid grid-cols-4 gap-2 justify-center items-center">
+                {form
+                  .watch("divisions")
+                  .sort((a, b) => a.division_id - b.division_id)
+                  .map((division, index) => (
+                    <FormField
+                      key={division.division_id}
+                      control={form.control}
+                      name={`divisions.${index}.active`}
+                      render={({ field }) => (
+                        <FormItem className="grid grid-cols-2 gap-2 w-full">
+                          <FormLabel className="min-w-fit text-sm items-center justify-center">
+                            {division.name}
+                          </FormLabel>
+                          <FormControl>
+                            <Checkbox
+                              checked={field.value}
+                              onCheckedChange={field.onChange}
+                              className="h-4 w-4 !mt-[2px] !mb-0 items-center justify-center"
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  ))}
+              </div>
             </div>
-          </div>
 
-          <Button type="submit">Save Settings</Button>
-        </form>
-      </Form>
+            <Button type="submit">Save Settings</Button>
+          </form>
+        </Form>
+      )}
     </div>
   );
 };
